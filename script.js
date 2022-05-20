397b0135e6c31e612ffcf2b042cd67d21694ed98
rafaelencinasr <rafaeladalbertoencinas94@gmail.com>
2022-05-19 17:26:40 -0700

    Add a better layout and title
    Moved all the buttons to the left of the drawing grid.
    Added a grey background.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ for(let i = 0 ; i<gridSize ; i++){
 }
 createGrid(gridSize);
 
-// Gets all the elements inside the outsideContainer that has the "pixel" class
+// Gets all the elements inside the outsideContainer that has the "pixelName" name
 // this is a live element, so it should update with each DOM change.
 
 let pixelNodeList = document.getElementsByName('pixelName');
@@ -40,12 +40,10 @@ pixelNodeList.forEach(div=>div.addEventListener('mouseover', ()=>{
 }));
 
 // Canvas clearing button, changes background color for all divs to white
-// to a white background.
-
 const clearCanvas = document.querySelector('.clearCanvas');
 clearCanvas.addEventListener('click', ()=>{
 
-    // For each div => remove the colorPixel class
+    // For each div => change background color to white
     pixelNodeList.forEach(div => div.setAttribute('style',`background: white;`));
     
 })
@@ -69,12 +67,17 @@ changeGridSize.addEventListener('click', ()=>{
         alert(`${gridSize}? That's too small, let's go with 1x1.`)
         gridSize=1;
     }
+    else if(isNaN(gridSize)){
+        alert(`"${gridSize}"? That's not a number, try again.`)
+        gridSize = prevGridSize;
+    }
     for(let l = 0; l < prevGridSize ; l++){
         let ripDivs = document.querySelector('.innerContainer');
         outsideContainer.removeChild(ripDivs);
     }
     createGrid(gridSize);
     //console.log(pixelNodeList);
+    // Adds the event listeners to the new divs
     pixelNodeList.forEach(div=>div.addEventListener('mouseover', ()=>{
         //console.log("hello");
         if(randomColorSelector){
@@ -92,20 +95,21 @@ function randomColorGen(){
     let randomDecimal = Math.floor(Math.random()*16777215);
     let decToHex = randomDecimal.toString(16);
     let randomColor = "#" + decToHex;
-    return randomColor;
+    return randomColor;     // returns a random color in hex
 }
+
 
 const changeToBlackColor = document.querySelector('#changeToBlackColor');
 const changeToRandomColor = document.querySelector('#changeToRandomColor');
 
-
+// Set color to black
 changeToBlackColor.addEventListener('click', ()=>{
     randomColorSelector = false;
     //console.log('changing to black');
     selectedColor = 'black';
 });
 
-
+// Set color to random by setting the flag "randomColorSelector" as true
 changeToRandomColor.addEventListener('click', ()=>{
     randomColorSelector = true;
 });
